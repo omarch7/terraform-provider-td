e7443e5490528ec5f0290103381568d4389b31b7
Omar Contreras <oc@hellofresh.com>
2025-02-27 11:21:53 +0100

    Create multiple folders across Parent Segments

--- examples/provider-install-verification/main.tf
+++ examples/provider-install-verification/main.tf
@@ -11,15 +11,22 @@ provider "td" {}
 data "td_parent_segments" "all" {}
 
 resource "td_folder" "test" {
-  name             = "1_TEST"
+  for_each = {
+    for parent_segment in data.td_parent_segments.all.parent_segments : parent_segment.id => parent_segment
+  }
+
+  name             = "1_TEST_TF"
   description      = "Test Description"
-  parent_folder_id = "956755"
+  parent_folder_id = each.value.parent_folder_id
 }
 
 resource "td_folder" "child" {
-  name             = "0_CHILD_UPDATE"
-  description      = "Child Description"
-  parent_folder_id = td_folder.test.id
+  for_each = {
+    for parent_segment in data.td_parent_segments.all.parent_segments : parent_segment.id => parent_segment
+  }
+  name             = "1_TEST_TF_CHILD_UPDATED"
+  description      = "Test Description"
+  parent_folder_id = td_folder.test[each.key].id
 }
 
 output "all_parent_segments" {
